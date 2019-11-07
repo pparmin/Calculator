a90b18ed5481b90e745797ef03d4b4b63211433f
pparmin <philipp.armingeon@googlemail.com>
2019-11-07 15:02:26 +0100

    Added mode functionality to operator buttons; Added full functionality

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,33 @@
+/* 
+***Basic functionality*** 
+
+The final calculation is made in operate(). When you press the results button, it will pass the saved Value and the current displayValue
+as well as the chosen operator to operate(). The function then calculates the result of that operation.
+
+
+At it's core, the calculator works with modes that define what kind of value
+the program is dealing with. 
+
+1. If the mode is "firstValue" (it is in the beginning), it is writing the numbers into the display
+and thus creating a displayValue to be used as the first value of the calculation.
+
+2. When you choose an operator, the operator takes the current display value, saves it in a variable, 
+and then sets the mode for all numbers on "secondValue".
+
+3. If the mode is "secondValue", the current display (and thus its value) will be cleared 
+as well as setting the mode back to "firstValue" mode. This enables the function to produce
+a second value (the current displayValue) which will eventually be used for the calculation, 
+together with the savedValue. 
+
+In order to chain operations, the program will look whether an operator has already been assigned (if class is "active").
+If it is, it will calculate a temporary result and display it, so it can be used in further calculations.
+*/
+
+
+
+/* Basic functions for the different types of calculation */
+/* ------------------------------------------------------ */
+
 function add (firstValue, secondValue) {
   let sum = Number(firstValue) + Number(secondValue);
   return sum;
@@ -14,49 +44,105 @@ function multiply (firstValue, secondValue) {
 }
 
 function divide (firstValue, secondValue) {
+  if (Number(secondValue) == 0) {
+    return 'You can not divide by 0';
+  }
+
   let division = Number(firstValue) / Number(secondValue);
   return division;
 }
 
+/* ------------------------------------------------------ */
+
+
+/* Function for the final calculation: 
+It clears the textbox and puts out a result instead, based on the operator type
+*/
 function operate (operator, firstValue, secondValue) {
-  console.log(operator);
-  console.log(typeof operator);
+  console.log(`Saved value: ${firstValue} & second value: ${secondValue}`);
 
   switch (operator) {
     case "add":
-      console.log('This is working');
       clearText();
       textbox.textContent += `${add(firstValue, secondValue)}`;
     break;
 
     case "subtract":
-      console.log('This is working');
       clearText();
       textbox.textContent += `${subtract(firstValue, secondValue)}`;
     break;
 
     case "multiply":
-      console.log('This is working');
       clearText();
       textbox.textContent += `${multiply(firstValue, secondValue)}`;
     break;
 
     case "divide":
-      console.log('This is working');
       clearText();
       textbox.textContent += `${divide(firstValue, secondValue)}`;
     break;
   }
+
+  // sets the mode to "secondValue"
+  setModeNumbers.setSecondValue();
+  displayValue = textbox.textContent;
+  console.log(`display value after result: ${displayValue}`);
 }
 
+
+
+// Saves the current value and changes the mode 
 function chooseOperator (event) {
 
-  // saving the currently displayed value
-  savedDisplay = displayValue;
-  console.log(`The current display value is: ${savedDisplay}`);
+  /* This checks whether the operator has already been activated and if it did it will calculate
+  a temporary result and display this value. From then on it will proceed with the normal function 
+  body. 
+
+  If operator is set to active, because it has already been used 
+    --> calculate temporary result and display it 
+      --> from there proceed normally with the updated operator value  
+
+  */
+
+  if (event.target.className == "active") {
+    console.log("-----IF PART ACTIVE-----")
+    console.log('Operator is active');
+    console.log(`OPERATOR CHOSEN: ${operator}`);
+    operate(operator, savedValue, displayValue);   
+  }
+
+
+  // saving the selected operator
   operator = event.target.value;
   console.log(`The chosen operator is: ${operator}`);
 
+  // saving the currently displayed value
+  savedValue = displayValue;
+  console.log(`The saved value is: ${savedValue}`);
+
+  // Sets the mode to "active"
+  setModeOperators.setActive();
+
+  // display the correct symbol on the textbox
+  addOperatorSymbol(operator);
+
+
+  // sets the mode to "secondValue"
+  setModeNumbers.setSecondValue();
+
+}
+
+function addNumber (event) {
+  if (event.target.classList == "secondValue") {
+    clearText();
+  }
+
+  textbox.textContent += event.target.value;
+  displayValue = textbox.textContent;
+  console.log(`NUMBER SELECTED: ${displayValue}`)
+}
+
+function addOperatorSymbol (operator) {
   switch (operator) {
     case "add":
       textbox.textContent += '+';
@@ -73,33 +159,57 @@ function chooseOperator (event) {
     case "multiply":
       textbox.textContent += '*';
     break; 
-
   }
-
-  numberButtons.forEach(button => {
-    button.classList.replace("inactive", "active");
-  });
 }
-
-function addNumber (event) {
-  if (event.target.classList == "active") {
-    event.target.classList.replace("active", "inactive");
-    clearText();
-  }
-
-  console.log(`old display value: ${displayValue}`);
-  console.log(event.target.value);
-  textbox.textContent += event.target.value;
-  displayValue = textbox.textContent;
-  console.log(`new display value: ${displayValue}`)
-}
-
+ 
 function clearText () {
   textbox.textContent = '';
+  setModeNumbers.setFirstValue();
 }
 
+// switches the operator mode back to "inactive"
+function clearOperators () {
+  setModeOperators.setInactive();
+}
+
+const setModeNumbers = {
+
+  setFirstValue : function() {
+    numberButtons.forEach(button => {
+      button.className = "firstValue";
+      //button.classList.replace("secondValue", "firstValue");
+    });
+  },
+
+  setSecondValue : function() {
+    numberButtons.forEach(button => {
+      button.classList.replace("firstValue", "secondValue");
+    });
+  },
+
+  setCalculated : function() {
+    numberButtons.forEach(button => {
+      button.className = 'calculated';
+    });
+  },
+};
+
+const setModeOperators = {
+
+  setActive : function() {
+    calcFunctions.forEach(operator => {
+      operator.className = "active";
+    });
+  },
+
+  setInactive : function() {
+    calcFunctions.forEach(operator => {
+      operator.className = "inactive";
+    });
+  }
+};
+
 const textbox = document.querySelector('.textbox');
-const calculator = document.querySelectorAll('.container');
 const result = document.querySelector('#result');
 
 const numberButtons = document.querySelectorAll('.numbers > button');
@@ -107,13 +217,12 @@ const calcFunctions = document.querySelectorAll('.calc-functions > button');
 console.log(numberButtons);
 console.log(calcFunctions);
 
-const seven = document.getElementById('seven');
 const clear = document.getElementById('clear');
 
-let displayValue = '0';
-let savedDisplay = '0';
+let displayValue = '';
+let savedValue = '';
+let tempResult = '';
 let operator = '';
-let mode = "inactive";
 
 numberButtons.forEach(button => {
   button.addEventListener('click', addNumber);
@@ -125,6 +234,17 @@ calcFunctions.forEach(operator => {
   operator.addEventListener('click', chooseOperator);
 });
 
-clear.addEventListener('click', clearText);
+clear.addEventListener('click', () => {
+  clearText();
+  clearOperators();
+});
 
-result.addEventListener('click', (event) => operate(operator, savedDisplay, displayValue));
+result.addEventListener('click', () => {
+  operate(operator, savedValue, displayValue);
+
+  // resetting the savedValue & operator after the operation fixes the issue
+  // of the result button switching between two values
+  savedValue = '';
+  operator = '';
+  setModeOperators.setInactive();
+});
